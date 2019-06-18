e1635468d8d9326023f41e5720969c0f60e4fe39
MasonHN <hunternmason@gmail.com>
2019-06-18 10:05:19 -0500

    changed host address

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -6,7 +6,7 @@ module.exports = {
   deploy: {
     production: {
       user: 'ubuntu',
-      host: 'ec2-18-216-220-130.us-east-2.compute.amazonaws.com',
+      host: 'ec2-18-224-173-211.us-east-2.compute.amazonaws.com',
       key: '~/.ssh/Product-Display.pem',
       ref: 'origin/master',
       repo: 'git@github.com:MasonHN/Product-Display.git',
